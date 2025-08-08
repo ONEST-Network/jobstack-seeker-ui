da49e73ec1b3c569bc33e0bdf3c2cf79884665e0
sudilate <amit.sarang@dhiway.com>
2025-08-08 13:34:53 +0530

    chore: some beautify in header of the page
    Signed-off-by: sudilate <amit.sarang@dhiway.com>

--- src/components/header/Header.tsx
+++ src/components/header/Header.tsx
@@ -37,7 +37,7 @@ const Header: React.FC<HeaderProps> = ({ orgSlug }) => {
   };
 
   return (
-    <header className="bg-white border-b border-border sticky top-0 z-50">
+    <header className="bg-gray-50/90 backdrop-blur-md border-b border-gray-200/60 sticky top-0 z-50">
       <div className="container mx-auto px-4 py-3">
         <div className="flex items-center justify-between">
           <OrgLogo orgSlug={orgSlug} />
